a541ad69211f2f8352f48c00969894f615deab84
drabikp <drabik.p@gmail.com>
2024-01-26 07:42:58 +0100

    Create damnForti.sh
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update damnForti.sh

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # damn-forti
-Simple SAML login script for FortiVPN
+Simple SAML login script for FortiVPN. 
+Heavily inspired by [fuckForticlient](https://github.com/nonamed01/fuckForticlient)'s attitude. 
+
+This would not be possible without [openfortivpn-webview](https://github.com/gm-vm/openfortivpn-webview) doing all the heavy lifting.
+
+For now, tested only on Manjaro Linux.
+Prerequisities:
+
+```
+yay -S openfortivpn openfortivpn-webview-qt
+```
+
+Modify variables ```VPN_HOST``` and ```TRUSTED_CERT``` to your needs.
---
+++ damnForti.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define VPN host and cookie file path
+VPN_HOST="vpn.example.com"
+COOKIE_FILE="$HOME/.config/fortivpn/cookie"
+TRUSTED_CERT=""
+
+# Function to connect to VPN
+connect_vpn() {
+    # Read cookie from file and pass it to openfortivpn
+    cookie=$(cat "$1")
+    sudo openfortivpn $VPN_HOST --cookie="$cookie" --trusted-cert $TRUSTED_CERT
+}
+
+# Check if cookie file exists
+if [[ -f "$COOKIE_FILE" ]]; then
+    echo "Cookie found, attempting to connect to VPN..."
+    connect_vpn "$COOKIE_FILE"
+
+    # Check if VPN connection was successful
+    if [[ $? -ne 0 ]]; then
+        echo "VPN connection failed. Running openfortivpn-webview to obtain new cookie..."
+        # Run openfortivpn-webview to get a new cookie
+        new_cookie=$(openfortivpn-webview $VPN_HOST)
+
+        # Store the new cookie
+        echo "$new_cookie" > "$COOKIE_FILE"
+
+        # Retry connection with new cookie
+        echo "Retrying VPN connection with new cookie..."
+        connect_vpn "$COOKIE_FILE"
+    else
+        echo "Connected to VPN successfully."
+    fi
+else
+    echo "No cookie found. Running openfortivpn-webview to obtain cookie..."
+    # The same procedure as above for obtaining and storing the cookie
+    new_cookie=$(openfortivpn-webview --get-cookie)
+    mkdir -p "$(dirname "$COOKIE_FILE")"
+    echo "$new_cookie" > "$COOKIE_FILE"
+
+    # Connect using the new cookie
+    connect_vpn "$COOKIE_FILE"
+fi
